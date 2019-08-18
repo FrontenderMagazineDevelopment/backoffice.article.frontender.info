8fc601995239befc672a3491686a554c1c3ab5ca
Anton Nemtsev <thesilentimp@gmail.com>
2019-08-18 20:57:20 +0200

    bundle size checker

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,54 @@
 import React from 'react';
-import Home from 'components/atoms';
+// import Home from 'components/atoms';
 
-export default ()=>(<p><Home /></p>);
+export default ()=>{
+  return (
+    <form action="/parse" methos="post">
+      <fieldset>
+        <label for="article-url">Введите URL статьи:</label>
+        <input 
+          autoFocus
+          id="article-url" 
+          name="url"
+          type="url"
+          placeholder="https://www.smashingmagazine.com/2019/07/essential-guide-javascript-newest-data-type-bigint/"
+          pattern="http(s)?://.*"
+          size="120"
+          required
+          spellcheckSection="false"
+           />
+        <button>сгенерировать slug</button>
+      </fieldset>
+      <fieldset>
+        <label for="article-url">Слаг:</label>
+        <input 
+          autoFocus
+          id="article-url" 
+          name="url"
+          type="url"
+          placeholder=""
+          pattern="http(s)?://.*"
+          size="80"
+          required
+          spellcheckSection="false"
+           />
+        <button>распарсить статью</button>
+      </fieldset>
+      <fieldset>
+        <label for="article-url">Добавить тэг:</label>
+        <input 
+          autoFocus
+          id="article-url" 
+          name="url"
+          type="url"
+          placeholder=""
+          pattern="http(s)?://.*"
+          size="80"
+          required
+          spellcheckSection="false"
+           />
+        <button>распарсить статью</button>
+      </fieldset>
+    </form>
+  );
+};
